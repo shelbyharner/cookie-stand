40205d093a988563d88627d4464efa23f2d6fd61
Shelby Harner <shelby.harner@gmail.com>
2021-01-26 22:11:29 -0800

    set up hours array and location customer objects

--- js/app.js
+++ js/app.js
@@ -2,3 +2,39 @@
 
 console.log('Hello Cookie Lovers');
 
+// let hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
+
+// let seattle = {
+//   name: 'Seattle',
+//   min: 23,
+//   max: 65,
+//   avg: 6.3,
+// };
+
+// let tokyo = {
+//   name: 'Tokyo',
+//   min: 3,
+//   max: 24,
+//   avg: 1.2,
+// };
+
+// let dubai = {
+//   name: 'Dubai',
+//   min: 11,
+//   max: 38,
+//   avg: 3.7,
+// };
+
+// let paris = {
+//   name: 'Paris',
+//   min: 20,
+//   max: 38,
+//   avg: 2.3,
+// };
+
+// let lima = {
+//   name: 'Lima',
+//   min: 2,
+//   max: 16,
+//   avg: 4.6,
+// };
